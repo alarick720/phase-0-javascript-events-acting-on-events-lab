a6467b0d11751add18e4d31e19ab04d0462d404b
Alarick Whitted <awhitted834@gmail.com>
2024-01-05 16:31:25 -0500

    done

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ document.addEventListener("keydown", function (e) {
   }   
     
   function moveDodgerRight() {
-    const dodger = document.getElementById('dodger'); 
+    
     const leftNumbers = dodger.style.left.replace('px', '');
     const left = parseInt(leftNumbers, 10);
   
     if (left < 360) { 
-      dodger.style.left = `${left + 1}px`;
+      dodger.style.left = `${left +1 }px`;
     }
   }
